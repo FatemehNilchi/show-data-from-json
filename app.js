bc34f83d681c7315cea165850f65783f65e9dead
FatemehNilchi <FatemehNilchi77@gmail.com>
2023-02-19 18:27:31 +0330

    first try with no output

---
+++ app.js
@@ -0,0 +1,18 @@
+fetch("products.json")
+.then(function(response){
+    return response.json();
+})
+.then(function(response){
+    let placeholder=document.querySelector("#data-output");
+    let out="";
+    for(let product of products){
+        out +=`
+        <tr>
+        <td> ${product.name}</td>
+        <td> ${product.family}</td>
+        <td> ${product.nomre}</td>
+        </tr>
+        `;
+    }
+    placeholder.innerHTML=out;
+})
